68c1668eebd7b5ab2b60aa47ac805d63bab08b01
galka <galka.new.york@gmail.com>
2023-09-08 10:53:50 -0400

    Stereotype Annotation

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
@@ -2,6 +2,7 @@ package com.cydeo.bean_annotation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration //when you start an application Spring will look for configuration class, go inside and create a bean for you and put in a container
 public class ConfigApp {
@@ -13,8 +14,15 @@ public class ConfigApp {
         return new FullTimeMentor();
     }
 
+   // @Bean(name = "p1")
     @Bean
+    @Primary
     PartTimeMentor partTimeMentor(){
+        return new PartTimeMentor();
+    }
+
+    @Bean
+    PartTimeMentor partTimeMentor2(){
         return new PartTimeMentor();
     }
 }
--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -7,13 +7,20 @@ public class CydeoApp {
 
     public static void main(String[] args) {
 
-        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
         // defining what kind of configuration method you'll use to define the beans (@Configuration)
-       FullTimeMentor ft = container.getBean(FullTimeMentor.class); // ft is a bean from the container
+        FullTimeMentor ft = container.getBean(FullTimeMentor.class); // ft is a bean from the container
+
+
+        PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+//        PartTimeMentor pt = container.getBean("p1",PartTimeMentor.class);
+
         ft.createAccount();
+        pt.createAccount();
 
         String str = container.getBean(String.class);
         System.out.println(str);
+
 
     }
 
