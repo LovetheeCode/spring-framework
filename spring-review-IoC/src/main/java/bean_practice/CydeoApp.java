94d3c5e005898b08002b9f09b2d8c4c0cc69dc0e
galka <galka.new.york@gmail.com>
2023-09-08 23:25:13 -0400

    week 1 review

---
+++ spring-review-IoC/src/main/java/bean_practice/CydeoApp.java
@@ -0,0 +1,19 @@
+package bean_practice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+        //how to create Spring container
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        FullTimeEmployee fullTimeEmployee = container.getBean(FullTimeEmployee.class);
+        fullTimeEmployee.createAccount();
+        PartTimeEmployee partTimeEmployee = container.getBean(PartTimeEmployee.class);
+        partTimeEmployee.createAccount();1
+
+
+    }
+}
--- spring-review-IoC/src/main/java/bean_practice/FullTimeEmployee.java
+++ spring-review-IoC/src/main/java/bean_practice/FullTimeEmployee.java
@@ -3,6 +3,6 @@ package bean_practice;
 public class FullTimeEmployee {
 
     public void createAccount(){
-        System.out.println("Fulltime employee is created.");
+        System.out.println("FullTime employee is created.");
     }
 }
