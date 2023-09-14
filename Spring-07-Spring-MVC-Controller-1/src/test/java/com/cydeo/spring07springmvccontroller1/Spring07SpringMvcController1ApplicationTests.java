216b41c609e6c9768332601b4c3b9b55b247d6f1
galka <galka.new.york@gmail.com>
2023-09-14 17:27:01 -0400

    unversioned

---
+++ Spring-07-Spring-MVC-Controller-1/src/test/java/com/cydeo/spring07springmvccontroller1/Spring07SpringMvcController1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring07springmvccontroller1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07SpringMvcController1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
