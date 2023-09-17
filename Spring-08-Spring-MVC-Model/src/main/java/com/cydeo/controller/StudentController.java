a48ae4ab736f3ac9058e974f1b3a52e4a815d24d
galka <galka.new.york@gmail.com>
2023-09-16 22:27:33 -0400

    Mentor

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,9 @@ public class StudentController {
         numbers.add(78);
         numbers.add(100);
         model.addAttribute("numbers",numbers);
+
+        Student student = new Student(1,"Mike","Smith");
+        model.addAttribute("student",student);
 
 
         return "student/welcome";
