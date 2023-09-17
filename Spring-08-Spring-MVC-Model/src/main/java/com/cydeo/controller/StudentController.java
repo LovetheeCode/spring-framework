56e22f13096fb2e154ec5acec8ce5cc147a21f8c
galka <galka.new.york@gmail.com>
2023-09-16 21:44:56 -0400

    Student Controller

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,17 +4,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model) {
 
         model.addAttribute("name", "Cydeo");
-        model.addAttribute("course","MVC");
+        model.addAttribute("course", "MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        int studentId = new Random().nextInt();
+        model.addAttribute("studentId",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(78);
+        numbers.add(100);
+        model.addAttribute("numbers",numbers);
 
 
         return "student/welcome";
     }
+
     @RequestMapping("/ozzy")
     public String homePage2(Model model) {
 
