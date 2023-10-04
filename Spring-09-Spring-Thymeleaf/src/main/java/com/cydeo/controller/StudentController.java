d7d6a69061a9ff83b28859d29ba3e08c7363cf11
galka <galka.new.york@gmail.com>
2023-10-03 22:02:56 -0400

    Spring Thymeleaf

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,13 +4,16 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller //because we want to create a bean
 @RequestMapping("/student") //better to put in class level
 public class StudentController {
 
-    @RequestMapping("/register")
+  //  @RequestMapping(value = "/register" , method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model) {
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -18,7 +21,8 @@ public class StudentController {
         return "student/register"; //--> return view (inside Student folder there is a register html), html file
     }
 
-    @RequestMapping("/welcome")
+    //@RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info() {
 
         return "student/welcome";
